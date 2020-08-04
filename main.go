2651c4c8baffb6f1898ecce352929c1fb0736f4e
Dmitriy <dubinin.dmitriy@gmail.com>
2020-08-04 10:17:03 +0300

    Moveout some part of code to better places

--- main.go
+++ main.go
@@ -32,14 +32,3 @@ func main() {
 	}
 
 }
-
-func getHeaders() []crss.CommunicationHeader {
-	return []crss.CommunicationHeader{
-		{"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"},
-		{"User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15"},
-		{"Host", "jobs.dou.ua"},
-		{"Accept-Language", "en-us"},
-		{"Accept-Encoding", "gzip, deflate, br"},
-		{"Connection", "keep-alive"},
-	}
-}
